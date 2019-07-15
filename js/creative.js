0711c3f79bacdeabaa7b09b7c85ccbddf12a8bfa
Alisson Lauffer <alissonvitortc@gmail.com>
2019-07-15 20:41:37 -0300

    Some changes
    - update mdb, jquery and fontawesome version
    - change accent 4285f4 to 1a73e8
    - change animations from 0.5s to 0.3s
    - increase navbar shadow size

--- js/creative.js
+++ js/creative.js
@@ -1,3 +1,12 @@
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+document.getElementById("my-age").innerHTML = calculate_age(new Date(2002, 05, 16));
+
 (function($) {
   "use strict"; // Start of use strict
 
