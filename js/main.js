290e7ae74ea53fc26dd312cf4af6543f503e2d31
Alisson Lauffer <alissonvitortc@gmail.com>
2021-06-03 06:31:26 -0300

    Add birthday counter

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ function scrolls() {
     scrollTop: $("#second").offset().top
 }, 1000);
 }
+
+function calculateAge(dob) {
+  let now = new Date();
+
+  var years = (now.getFullYear() - dob.getFullYear());
+
+  if (now.getMonth() < dob.getMonth() || now.getMonth() == dob.getMonth() && now.getDate() < dob.getDate()) {
+    years--;
+  }
+
+  return years;
+}
+
+document.getElementById("my-age").innerText = calculateAge(new Date(2002, 4, 16));  // My real birthday is 2002/05/16, but JavaScript count months from 0...
