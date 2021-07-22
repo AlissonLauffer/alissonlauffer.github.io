f133e6db546e6870defeb8638223f481d9aa89a4
Alisson Lauffer <alissonvitortc@gmail.com>
2021-07-22 02:40:30 -0300

    Drop jQuery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 window.onscroll = function(){
   var pos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
   if (pos > 200){
-    $("#bottom").fadeOut()
+    document.getElementById("bottom").classList.add("invisible");
   }
   else {
-    $("#bottom").fadeIn()
+    document.getElementById("bottom").classList.remove("invisible");
   }
-}
-
-function scrolls() {
-  $("html, body").animate({
-    scrollTop: $("#second").offset().top
-}, 1000);
 }
 
 function calculateAge(dob) {
