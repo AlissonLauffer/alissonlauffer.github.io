07be4bc7005abf83666506160a4156ed21c962d0
Alisson Lauffer <alissonvitortc@gmail.com>
2023-08-22 22:58:10 -0300

    chore: bump dependencies and do some misc changes

--- src/ts/faIcons.ts
+++ src/ts/faIcons.ts
@@ -1,6 +1,6 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faGitlab, faStackOverflow, faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faGitlab, faStackOverflow, faTelegram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faGithub, faGitlab, faStackOverflow, faTelegram, faTwitter);
+library.add(faGithub, faGitlab, faStackOverflow, faTelegram, faXTwitter);
 
 dom.watch();
