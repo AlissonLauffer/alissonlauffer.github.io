2fb26f124da7fe918e4fad9ef0bde4c84af59866
Alisson Lauffer <alissonvitortc@gmail.com>
2024-07-10 20:51:23 -0300

    initial migration to astro

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# My personal website
+# Alisson's personal website
+
+## Building
+
+```shell
+bun install
+bun run build
+```
+
+Build results are in the `dist` directory.
+
+## Testing
+
+```shell
+bun run dev
+```
--- src/ts/faIcons.ts
+++
@@ -1,6 +0,0 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faGitlab, faLinkedin, faStackOverflow, faTelegram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faGithub, faGitlab, faLinkedin, faStackOverflow, faTelegram, faXTwitter);
-
-dom.watch();
